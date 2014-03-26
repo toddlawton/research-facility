4993f5060393c08eea5c2488f0f6dac7392f393f
toddlawton <toddbee@gmail.com>
2014-03-25 21:54:18 -0400

    Monkeying around with Angular

--- base/js/app.js
+++ base/js/app.js
@@ -1,32 +1,59 @@
-var feeds = [];
-	
-angular.module('feedModule', ['ngResource'])
-.factory('FeedLoader', function ($resource) {
-	return $resource('http://ajax.googleapis.com/ajax/services/feed/load', {}, {
-		fetch: { method: 'JSONP', params: {v: '1.0', callback: 'JSON_CALLBACK'} }
+var app = angular.module("app", ['ngRoute']).config(function($routeProvider){
+	$routeProvider.when('/login', {
+		templateUrl: 'login.html',
+		controller: 'LoginController'
 	});
-})
-.service('FeedList', function ($rootScope, FeedLoader) {
-	this.get = function() {
-		var feedSources = [
-			{title: 'Reddit', url: 'http://www.reddit.com/.rss'},
-			{title: 'Tweakers', url: 'http://feeds.feedburner.com/tweakers/mixed'},
-			{title: 'Wired', url: 'http://feeds.wired.com/wired/index'},
-		];
-		if (feeds.length === 0) {
-			for (var i=0; i<feedSources.length; i++) {
-				FeedLoader.fetch({q: feedSources[i].url, num: 10}, {}, function (data) {
-					var feed = data.responseData.feed;
-					feeds.push(feed);
-				});
+
+	$routeProvider.when('/home', {
+		templateUrl: 'home.html',
+		controller: 'HomeController'
+	});
+
+	$routeProvider.otherwise({ redirectTo: '/login' });
+});
+
+app.factory("AuthenticationService", function($location) {
+	return {
+		login: function(credentials) {
+			if (credentials.username === "admin") {
+				$location.path('/home');
 			}
+		},
+		logout: function() {
+			$location.path('/login');
 		}
-		return feeds;
+	}
+});
+
+app.controller('LoginController', function($scope, AuthenticationService) {
+	$scope.credentials = { username: "", password: "" };
+	
+	$scope.login = function() {
+		AuthenticationService.login($scope.credentials);
 	};
-})
-.controller('FeedCtrl', function ($scope, FeedList) {
-	$scope.feeds = FeedList.get();
-	$scope.$on('FeedList', function (event, data) {
-		$scope.feeds = data;
-	});
+});
+
+app.controller('HomeController', function($scope, AuthenticationService) {
+	$scope.message = "Hover to see a sample message!";
+
+	$scope.logout = function() {
+		AuthenticationService.logout();
+	}
+});
+
+app.directive('messageOnHover', function() {
+	return {
+		restrict: "A", // A = Attribute, C = Class, E = Element, M = HTML Comment
+		link: function(scope, element, attributes) {
+			var originalMessage = scope.message;
+			element.bind("mouseover", function() {
+				scope.message = attributes.message;
+				scope.$apply();
+			});
+			element.bind("mouseout", function() {
+				scope.message = originalMessage;
+				scope.$apply();
+			});
+		}
+	};
 });
